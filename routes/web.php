a5e5f5f262ca86678d9576c22c3bbe1438044e9b
darmawanmlnaa <darmawanmlnaa@gmail.com>
2024-03-25 03:05:27 +0700

    feat: optimize web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\ExperienceController;
-use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\Web\ExperienceController;
+use App\Http\Controllers\Web\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
